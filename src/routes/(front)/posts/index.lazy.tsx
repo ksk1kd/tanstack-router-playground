fd5a2881aad6b4f3d47b32d08529c3e355b37f87
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-05-30 01:17:55 +0900

    add dynamic link

--- src/routes/(front)/posts/index.lazy.tsx
+++ src/routes/(front)/posts/index.lazy.tsx
@@ -1,4 +1,4 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/(front)/posts/')({
   component: RouteComponent,
@@ -11,6 +11,21 @@ function RouteComponent() {
     <>
       <h1>Posts</h1>
       <div>foo: {foo}</div>
+      <ul>
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((id) => (
+          <li key={id}>
+            <Link
+              to="/posts/$postId"
+              params={{
+                postId: id.toString(),
+              }}
+              className="[&.active]:font-bold"
+            >
+              Post {id}
+            </Link>{' '}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
